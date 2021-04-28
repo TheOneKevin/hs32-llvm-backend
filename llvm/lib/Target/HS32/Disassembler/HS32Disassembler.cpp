15e2cf9835229aa82b765a309ef26c0c294fe2cf
TheOneKevin <kevindai02@outlook.com>
2021-04-27 21:40:54 -0400

    [HS32] Fixed disassembler
    - Disassembler must first normalize instruction input, zeroing the ignored fields.

--- llvm/lib/Target/HS32/Disassembler/HS32Disassembler.cpp
+++ llvm/lib/Target/HS32/Disassembler/HS32Disassembler.cpp
@@ -75,6 +75,14 @@ static DecodeStatus decodeSImmOperand(MCInst &Inst, uint64_t Imm,
 // HS32Disassembler class methods
 //===----------------------------------------------------------------------===//
 
+#define OP_MASK 0xFF00'0000
+#define RD_MASK 0x00F0'0000
+#define RM_MASK 0x000F'0000
+#define SH_MASK 0x0000'0FFF
+#define OP_F_MASK 0b111'00'000
+#define OP_G_MASK 0b000'11'000
+#define OP_V_MASK 0b000'00'111
+
 DecodeStatus HS32Disassembler::getInstruction(MCInst &Instr, uint64_t &Size,
                                               ArrayRef<uint8_t> Bytes,
                                               uint64_t Address,
@@ -85,6 +93,55 @@ DecodeStatus HS32Disassembler::getInstruction(MCInst &Instr, uint64_t &Size,
     return MCDisassembler::Fail;
   }
   uint32_t Insn = support::endian::read32be(Bytes.data());
+
+  // Get opcode and family, group, var
+  uint8_t Op = (Insn & OP_MASK) >> 24;
+  uint8_t Opf = (Op & OP_F_MASK) >> 5;
+  uint8_t Opg = (Op & OP_G_MASK) >> 3;
+  uint8_t Opv = (Op & OP_V_MASK);
+
+  // Normalize unused fields
+  // TODO: Change when we implement shift
+  // ALU_rr
+  if(Opf >= 0b010 && Opf <= 0b110 && Opg == 0 && (Opv & 0b100) == 0) {
+    Insn = Insn & ~SH_MASK;
+  }
+  // BCC_pi and LCC_pi
+  else if((Op & 0xF0) == 0b0101'0000 || (Op & 0xF0) == 0b0111'0000) {
+    Insn = Insn & ~RD_MASK;
+    Insn = Insn & ~RM_MASK;
+  }
+  // All other instructions
+  else {
+    switch (Op) {
+    // RLDR and RSTR
+    case 0b000'10'001:
+    case 0b001'10'001:
+      Insn = Insn & ~SH_MASK;
+      break;
+
+    // RMOV and IMOV
+    case 0b001'00'000:
+      Insn = Insn & ~SH_MASK;
+      LLVM_FALLTHROUGH;
+    case 0b001'00'100:
+      Insn = Insn & ~RM_MASK;
+      break;
+
+    // RCMP and RTST
+    case 0b011'01'000:
+    case 0b100'01'000:
+      Insn = Insn & ~SH_MASK;
+      LLVM_FALLTHROUGH;
+    // ICMP and ITST
+    case 0b011'01'100:
+    case 0b100'01'100:
+      Insn = Insn & ~RD_MASK;
+      break;
+    }
+  }
+
+  // Finally parse
   return decodeInstruction(DecoderTable32, Instr, Insn,
                            Address, this, STI);
 }
